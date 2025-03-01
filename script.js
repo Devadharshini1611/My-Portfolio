611031dc9b72cf9164f2d0d4a8da6775fa17bee2
Deva-16400 <rameshdharshini400@gmail.com>
2025-03-01 14:55:58 +0530

    Updated project with new changes

--- script.js
+++ script.js
@@ -76,3 +76,14 @@ document.addEventListener("DOMContentLoaded", function() {
             });
     });
 });
+<button class="download-btn" onclick="downloadCV()">Download CV</button>
+
+    function downloadCV() {
+        const link = document.createElement('a');
+        link.href = 'cv.pdf';
+        link.download = 'Devadharshini_CV.pdf';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
+
